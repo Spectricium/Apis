ef17f18b9af6e602482d84916119d7cc4bb895dc
Spectricium <113564281+Spectricium@users.noreply.github.com>
2022-10-15 20:35:48 -0500

    Update and rename .gitignore to SpectraciumInfiniteUIApi.lua

---
+++ SpectraciumInfiniteUIApi.lua
@@ -0,0 +1,32 @@
+if Input == "/Speed" then
+  if Value == "" then
+  warn("Value Its Empty Need A Value")
+    else
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+  end
+  end
+if Input == "/Jump" then
+  if Value == "" then
+  warn("Value Its Empty Need A Value")
+    else
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+  end
+  end
+if Input == "/Die" then
+    game.Players.LocalPlayer.Character:FindFirstChild("Humanoid").Health = 0
+  end
+if Input == "/Health" then
+  if Value == "" then
+  warn("Value Its Empty Need A Value")
+    else
+    game.Players.LocalPlayer.Character.Humanoid.Health = Value
+  end
+  end
+
+if Input == "/MaxHealth" then
+  if Value == "" then
+  warn("Value Its Empty Need A Value")
+    else
+    game.Players.LocalPlayer.Character.Humanoid.MaxHealth = Value
+  end
+  end
